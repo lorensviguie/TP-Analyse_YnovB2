44ff15e514acd14ae5b70dcb5f3e569d3981ae4f
lorensviguie <lorens.viguie@ynov.com>
2024-03-29 16:05:06 +0100

    merge is hard

--- work/main/create_csv.go
+++ work/main/create_csv.go
@@ -1,50 +1,148 @@
 package main
 
 import (
-	// "encoding/csv"
-	"fmt"
-	// "log"
-	// "os"
-	// "strconv"
+	"encoding/csv"
+	"log"
+	"os"
+	"strconv"
 	"time"
 	"work/dice"
 )
 
 func main() {
-	// // Ouvrir le fichier CSV en mode ajout
-	// file, err := os.OpenFile("donnees.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	// if err != nil {
-	// 	log.Fatalf("Impossible d'ouvrir le fichier : %s", err)
-	// }
-	// defer file.Close()
-
-	// // Créer un écrivain CSV
-	// writer := csv.NewWriter(file)
-	// defer writer.Flush()
-
-	// // Effectuer 1000 lancers de dé pour chaque rang du dé
-	// for rank := 1; rank <= 10; rank++ {
-	// 	for i := 0; i < 1000; i++ {
-	// 		seed := time.Now().UnixNano()
-	// 		result := dice.Roll_Parabole_Dice(rank, seed)
-
-	// 		row := []string{
-	// 			"parabole_dice",
-	// 			strconv.FormatInt(seed, 10),
-	// 			getDiceName(rank),
-	// 			strconv.Itoa(result),
-	// 		}
-
-	// 		if err := writer.Write(row); err != nil {
-	// 			log.Fatalf("Erreur lors de l'écriture des données dans le fichier : %s", err)
-	// 		}
-	// 	}
-	// }
-
-	// log.Println("Données ajoutées au fichier CSV avec succès.")
-	for i := 0; i < 1000; i++ {
-		fmt.Println(dice.Roll_Parabole_Dice(1, time.Now().UnixNano()))
+	// Ouvrir le fichier CSV en mode ajout
+	file, err := os.OpenFile("donnees.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Impossible d'ouvrir le fichier : %s", err)
 	}
+	defer file.Close()
+
+	// Créer un écrivain CSV
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Effectuer 1000 lancers de dé pour chaque rang du dé
+	for rank := 1; rank <= 10; rank++ {
+		for i := 0; i < 1000; i++ {
+			seed := time.Now().UnixNano()
+			result := dice.Roll_Base_Dice(rank, seed)
+
+			row := []string{
+				"base_dice",
+				strconv.FormatInt(seed, 10),
+				getDiceName(rank),
+				strconv.Itoa(result),
+			}
+
+			if err := writer.Write(row); err != nil {
+				log.Fatalf("Erreur lors de l'écriture des données dans le fichier : %s", err)
+			}
+		}
+	}
+	for rank := 1; rank <= 10; rank++ {
+		for i := 0; i < 1000; i++ {
+			seed := time.Now().UnixNano()
+			result := dice.Roll_NormalDice(rank, seed)
+
+			row := []string{
+				"normal_dice",
+				strconv.FormatInt(seed, 10),
+				getDiceName(rank),
+				strconv.Itoa(result),
+			}
+
+			if err := writer.Write(row); err != nil {
+				log.Fatalf("Erreur lors de l'écriture des données dans le fichier : %s", err)
+			}
+		}
+	}
+	for rank := 1; rank <= 10; rank++ {
+		for i := 0; i < 1000; i++ {
+			seed := time.Now().UnixNano()
+			result := dice.Roll_Parabole_Dice(rank, seed)
+
+			row := []string{
+				"parabole_dice",
+				strconv.FormatInt(seed, 10),
+				getDiceName(rank),
+				strconv.Itoa(result),
+			}
+
+			if err := writer.Write(row); err != nil {
+				log.Fatalf("Erreur lors de l'écriture des données dans le fichier : %s", err)
+			}
+		}
+	}
+	for rank := 1; rank <= 10; rank++ {
+		for i := 0; i < 1000; i++ {
+			seed := time.Now().UnixNano()
+			result := dice.Power_Dice(rank, seed)
+
+			row := []string{
+				"power_dice",
+				strconv.FormatInt(seed, 10),
+				getDiceName(rank),
+				strconv.Itoa(result),
+			}
+
+			if err := writer.Write(row); err != nil {
+				log.Fatalf("Erreur lors de l'écriture des données dans le fichier : %s", err)
+			}
+		}
+	}
+	for rank := 1; rank <= 10; rank++ {
+		for i := 0; i < 1000; i++ {
+			seed := time.Now().UnixNano()
+			result := dice.Roll_RankDice(rank, seed)
+
+			row := []string{
+				"rank_dice",
+				strconv.FormatInt(seed, 10),
+				getDiceName(rank),
+				strconv.Itoa(result),
+			}
+
+			if err := writer.Write(row); err != nil {
+				log.Fatalf("Erreur lors de l'écriture des données dans le fichier : %s", err)
+			}
+		}
+	}
+	for rank := 1; rank <= 10; rank++ {
+		for i := 0; i < 1000; i++ {
+			seed := time.Now().UnixNano()
+			result := dice.Roll_Scaledice(rank, seed)
+
+			row := []string{
+				"scale_dice",
+				strconv.FormatInt(seed, 10),
+				getDiceName(rank),
+				strconv.Itoa(result),
+			}
+
+			if err := writer.Write(row); err != nil {
+				log.Fatalf("Erreur lors de l'écriture des données dans le fichier : %s", err)
+			}
+		}
+	}
+	for rank := 1; rank <= 10; rank++ {
+		for i := 0; i < 1000; i++ {
+			seed := time.Now().UnixNano()
+			result := dice.Roll_unscaledice(rank, seed)
+
+			row := []string{
+				"unscale_dice",
+				strconv.FormatInt(seed, 10),
+				getDiceName(rank),
+				strconv.Itoa(result),
+			}
+
+			if err := writer.Write(row); err != nil {
+				log.Fatalf("Erreur lors de l'écriture des données dans le fichier : %s", err)
+			}
+		}
+	}
+
+	log.Println("Données ajoutées au fichier CSV avec succès.")
 }
 
 // getDiceName retourne le nom du dé en fonction de son rang
